e64ef82dd7dffc72a3de690e1309f25e44f22571
Sambit Mondal <sambitmondal2005@gmail.com>
2026-05-18 12:23:51 +0530

    LC567_SW

---
+++ Sliding Window/LC_567.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int s1Freq[] = new int[26];
+        int windowFreq[] = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Freq[s1.charAt(i) - 'a']++;
+            windowFreq[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = s1.length(); i < s2.length(); i++) {
+            if (matches(s1Freq, windowFreq)) {
+                return true;
+            }
+
+            windowFreq[s2.charAt(i) - 'a']++;
+            windowFreq[s2.charAt(i - s1.length()) - 'a']--;
+        }
+
+        return matches(s1Freq, windowFreq);
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// TC: O(n)
+// SC: O(1)
