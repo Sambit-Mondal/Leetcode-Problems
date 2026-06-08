8eb298bb4ee539a095cf9d35eb2a505776138f09
Sambit Mondal <sambitmondal2005@gmail.com>
2026-06-08 18:41:07 +0530

    LC543_Trees

--- Trees/LC_543.java
+++ Trees/LC_543.java
@@ -14,24 +14,24 @@
  * }
  */
 class Solution {
-    int res = 0;
+    int maxDiameter = 0;
 
     public int diameterOfBinaryTree(TreeNode root) {
         dfs(root);
-        return res;
+        return maxDiameter;
     }
 
-    private int dfs(TreeNode root) {
-        if (root == null) {
+    private int dfs (TreeNode node) {
+        if (node == null) {
             return 0;
         }
 
-        int l = dfs(root.left);
-        int r = dfs(root.right);
+        int left = dfs(node.left);
+        int right = dfs(node.right);
 
-        res = Math.max(res, l + r);
+        maxDiameter = Math.max(maxDiameter, left + right);
 
-        return 1 + Math.max(l, r);
+        return 1 + Math.max(left, right);
     }
 }
 
