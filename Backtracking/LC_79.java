d5fbab970b149d9f1bcb22b6853d94a69a238cf3
Sambit Mondal <sambitmondal2005@gmail.com>
2026-06-12 21:35:48 +0530

    Update: LC79_Backtracking

--- Backtracking/LC_79.java
+++ Backtracking/LC_79.java
@@ -1,11 +1,14 @@
 class Solution {
+    int m, n;
+    int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
+        m = board.length;
+        n = board[0].length;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (backtrack(board, word, i, j, 0)) {
+                if (board[i][j] == word.charAt(0) && backtrack(board, i, j, 0, word)) {
                     return true;
                 }
             }
@@ -14,28 +17,36 @@ class Solution {
         return false;
     }
 
-    private boolean backtrack(char[][] board, String word, int i, int j, int k) {
-        if (k == word.length()) {
+    private boolean backtrack(char[][] board, int i, int j, int idx, String word) {
+        if (idx == word.length()) {
             return true;
         }
 
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(k)) {
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] == '$') {
+            return false;
+        }
+
+        if (board[i][j] != word.charAt(idx)) {
             return false;
         }
 
         char temp = board[i][j];
-        board[i][j] = '#';
+        board[i][j] = '$';
 
-        boolean found = backtrack(board, word, i + 1, j, k + 1) ||
-                        backtrack(board, word, i - 1, j, k + 1) ||
-                        backtrack(board, word, i, j + 1, k + 1) ||
-                        backtrack(board, word, i, j - 1, k + 1);
-        
+        for (int[] direction : directions) {
+            int new_i = i + direction[0];
+            int new_j = j + direction[1];
+
+            if (backtrack(board, new_i, new_j, idx + 1, word)) {
+                return true;
+            }
+        }
+
         board[i][j] = temp;
         
-        return found;
+        return false;
     }
 }
 
-// TC: O(m * n * (4 ^ L))
-// SC: O(L)
+// TC: O(m * n * (3 ^ l)) [l = length of word]
+// SC: O(l)
