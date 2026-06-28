0ef8bfaa8153e928abe533226fd5ccc58005a848
Sambit Mondal <sambitmondal2005@gmail.com>
2026-06-29 00:03:23 +0530

    LC1846_Greedy

---
+++ Greedy/LC_1846.java
@@ -0,0 +1,25 @@
+class Solution {
+    private static final int[] freq = new int[100005];
+
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        int n = arr.length;
+
+        for (int a : arr) {
+            freq[Math.min(a, n)]++;
+        }
+
+        int res = 1;
+        for (int i = 2; i <= n; i++) {
+            res = Math.min(res + freq[i], i);
+        }
+
+        for (int i = 0; i <= n; i++) {
+            freq[i] = 0;
+        }
+
+        return res;
+    }
+}
+
+// TC: O(n)
+// SC: O(n)
