f1bd78ada345c3972338368bd48a50135737a91a
Sambit Mondal <sambitmondal2005@gmail.com>
2026-08-08 14:21:29 +0530

    LC40_Backtracking

---
+++ Backtracking/LC_40.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack (int[] candidates, int target, int start, List<Integer> temp, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            if (candidates[i] > target) {
+                break;
+            }
+
+            temp.add(candidates[i]);
+            backtrack(candidates, target - candidates[i], i + 1, temp, res);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
+
+// TC: O(2 ^ n)
+// SC: O(n)
